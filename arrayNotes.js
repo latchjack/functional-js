4fbff152d57ce75eba5d47f8f83076f67eee04f1
Latch Jackn <latch.jack@gmail.com>
2021-07-16 16:36:45 +0100

    updating notes

--- arrayNotes.js
+++ arrayNotes.js
@@ -56,3 +56,115 @@ function updateDescription(meal) {
     };
     return meal;
 };
+
+//   To remove an item from the array we can use the filter function.
+const filteredMeals = updatedMeals.filter(function(meal) {
+    return meal.id !== 1;
+});
+
+/*
+|   This would remove the meal with the id of '1' from the array.
+|   Instead of passing a named function we passed an anonymous
+|   function to the filter function. We then passed the meal as a
+|   parameter.
+*/
+
+/*
+|   To summarise data in an array in an immutable way, we can use
+|   the Reduce function.
+|
+|   Reduce is similar to map and filter because it expects transformation\
+|   function as a paratemer.
+|
+|   In our sum function it takes the numbers array and takes 
+|   const numbers = [1 (x), 2 (y), 3];
+|
+|   function sum (x , y) {
+|       return x + y;
+|   };
+|   
+|   X + Y gives us '3'. It then moves onto the next index in the array '3'.
+|   The total of the last x + y becomes our 'x' this time around which is
+|   named the accumulator. So it would be 3 + 3, giving us the total of '6'.
+*/
+
+//  Example
+const numbers = [1, 2, 3];
+function sum(x, y) {
+    return x + y;
+};
+
+const total = numbers.reduce(sum);
+
+/*
+|   1. Find the average grade from all of the below scores.
+|   2. To find out how many students scored A's, B's, C's, D's,
+|   E's and F's.
+*/
+const grades = [60, 55, 80, 90, 99, 92, 75, 72];
+
+// 1.
+const total = grades.reduce(sum);
+
+function sum(x, y) {
+    return x + y;
+};
+
+/*
+|   Which would give us 623. 
+|   To calculate the average we just need to divide the total
+|   by the number of grades.
+*/
+const count = grades.length;
+
+//  console.log(total/count), which would give us 77.875
+
+//  2.
+
+/*
+|   The object at the end of the reduce function will make
+|   the accumulator as an empty object when it initialises.
+*/
+const letterGradeCount = grades.reduce(groupByGrade, {});
+
+function groupByGrade(acc, grade) {
+    if (grade >= 90) {
+        return {...acc, a: acc.a + 1};
+    }
+};
+
+/*
+|   On the first challenge the accumulator was a number that
+|   increased as each element in the array was evaluated.
+|   However the accumulator doesn't have to be a number. To
+|   specify an initial or starting accumulator value, that 
+|   the reducer should use, you pass in a second parameter
+|   to the reduce function.
+|
+|   e.g.
+|
+|   const total = numbers.reduce(sum, ?);
+|
+|   This second parameter is passed into the sum function as
+|   the first parameter (the acc) and the first parameter 
+|   becomes the second.
+|
+|   const total = numbers.reduce(sum, 5);
+|   const numbers = [1, 2, 3];
+|
+|   function sum(x (5), y (1)) {
+|       return x + y;
+|   }
+|
+|   Which would make the final total '11' instead of the original
+|   '6' result.
+*/
+
+
+
+/*
+|   Add to an array = Spread operator
+|   Transform an array = Map function
+|   Remove from an array = filter function
+|   Summarise information in an array = Reduce function
+*/

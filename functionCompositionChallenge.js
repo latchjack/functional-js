37d642febd18ded239f406195bef2596978e78dc
Latch Jackn <latch.jack@gmail.com>
2021-11-01 21:20:41 +0000

    Added new file

---
+++ functionCompositionChallenge.js
@@ -0,0 +1,15 @@
+// Count how many digits there are in the following 
+// sentence, using functional composition
+
+// NOTE: If you get stuck, you can get some hints from 
+// the following jsbin: 
+// https://jsbin.com/jokefus/2/edit?js,console
+// my solution is here: https://jsbin.com/duxewec/1/edit?js,console
+
+const sentence = 'PechaKucha is a presentation style in which 20 slides are shown for 20 seconds each (6 minutes and 40 seconds in total).';
+
+const numbersInString = // add function composition here
+
+expect(numbersInString(sentence)).toBe(7); 
+
+console.log('If you see this printed in the console, the test passed!');
